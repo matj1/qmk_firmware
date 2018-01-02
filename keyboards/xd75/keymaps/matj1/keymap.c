6eaef34d6b91ff1beb80ef867dd7a1a90a345bb2
matj1 <matej.bjgar@atlas.cz>
2018-01-02 18:05:45 +0100

    prohození lalt a app kláves

--- keyboards/xd75/keymaps/matj1/keymap.c
+++ keyboards/xd75/keymaps/matj1/keymap.c
@@ -19,7 +19,7 @@
 #define _QW 0
 #define _COL 1
 #define _FN 2
-#define _SYS 3
+#define _KBD 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -33,16 +33,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | \      | Z      | X      | C      | V      | B      | [      | UP     | ]      | N      | M      | ,      | .      | /      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | LCTRL  | LGUI   | APP    | LALT   | FN0    | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | RALT   | FN1    | KEYBD  | RGUI   | RCTRL  |
+ * | LCTRL  | LGUI   | LALT   | APP    | FN0    | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | RALT   | FN1    | KEYBD  | RGUI   | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_QW] = { /* QWERTY */
-  { KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,   KC_INS,  KC_PGUP, KC_HOME, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC },
-  { KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,   KC_DEL,  KC_PGDN, KC_END,  KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_ENT  },
-  { KC_LSFT, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,   KC_MINS, KC_GRV,  KC_EQL,  KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_RSFT },
-  { KC_BSLS, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,   KC_LBRC, KC_UP,   KC_RBRC, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_QUOT },
-  { KC_LCTL, KC_LGUI, KC_APP, KC_LALT, MO(_FN), KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC, KC_RALT, MO(_FN), MO(_KBD), KC_RGUI, KC_RCTL },
+  { KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,   KC_INS,  KC_PGUP, KC_HOME, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,   KC_DEL,  KC_PGDN, KC_END,  KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_ENT  },
+  { KC_LSFT, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,   KC_MINS, KC_GRV,  KC_EQL,  KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_RSFT },
+  { KC_BSLS, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,   KC_LBRC, KC_UP,   KC_RBRC, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_QUOT },
+  { KC_LCTL, KC_LGUI, KC_LALT, KC_APP, MO(_FN), KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC, KC_RALT, MO(_FN), MO(_KBD), KC_RGUI, KC_RCTL },
  },
 
 /* COLEMAK
@@ -55,16 +55,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | \      | Z      | X      | C      | V      | B      | [      | UP     | ]      | K      | M      | ,      | .      | /      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | LCTRL  | LGUI   | APP    | LALT   | _FN    | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | RALT   | _FN    | SYSTEM | RGUI   | RCTRL  |
+ * | LCTRL  | LGUI   | LALT   | APP    | _FN    | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | RALT   | _FN    | KEYBD  | RGUI   | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_COL] = { /* COLEMAK */
-  { KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,   KC_INS,  KC_PGUP, KC_HOME, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC },
-  { KC_TAB,  KC_Q,    KC_W,   KC_F,    KC_P,    KC_G,   KC_DEL,  KC_PGDN, KC_END,  KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_ENT  },
-  { KC_LSFT, KC_A,    KC_R,   KC_S,    KC_T,    KC_D,   KC_MINS, KC_GRV,  KC_EQL,  KC_H,   KC_N,    KC_E,    KC_I,    KC_O,     KC_RSFT },
-  { KC_BSLS, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,   KC_LBRC, KC_UP,   KC_RBRC, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_QUOT },
-  { KC_LCTL, KC_LGUI, KC_APP, KC_LALT, MO(_FN), KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC, KC_RALT, MO(_FN), MO(_KBD), KC_RGUI, KC_RCTL },
+  { KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,   KC_INS,  KC_PGUP, KC_HOME, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC },
+  { KC_TAB,  KC_Q,    KC_W,    KC_F,   KC_P,    KC_G,   KC_DEL,  KC_PGDN, KC_END,  KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_ENT  },
+  { KC_LSFT, KC_A,    KC_R,    KC_S,   KC_T,    KC_D,   KC_MINS, KC_GRV,  KC_EQL,  KC_H,   KC_N,    KC_E,    KC_I,    KC_O,     KC_RSFT },
+  { KC_BSLS, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,   KC_LBRC, KC_UP,   KC_RBRC, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_QUOT },
+  { KC_LCTL, KC_LGUI, KC_LALT, KC_APP, MO(_FN), KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC, KC_RALT, MO(_FN), MO(_KBD), KC_RGUI, KC_RCTL },
  },
 
 /* FUNCTION
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |        |        |        |        |        |        | UP     |        | .      | 1      | 2      | 3      | *      | /      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | LCTRL  | LGUI   | APP    | LALT   |        |        | LEFT   | DOWN   | RIGHT  | 0      | RALT   |        |        | RGUI   | RCTRL  |
+ * | LCTRL  | LGUI   | LALT   | APP    |        |        | LEFT   | DOWN   | RIGHT  | 0      | RALT   |        |        | RGUI   | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -86,10 +86,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_DEL,  KC_PGDN, KC_END,  KC_TRNS, KC_7,    KC_8,    KC_9,    KC_PMNS, KC_ENT  },
   { KC_LSFT, KC_TRNS, KC_VOLD, KC_MUTE, KC_VOLU, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_PPLS, KC_RSFT },
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_DOT,  KC_1,    KC_2,    KC_3,    KC_PAST, KC_PSLS },
-  { KC_LCTL, KC_LGUI, KC_APP,  KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_0,    KC_RALT, KC_TRNS, KC_TRNS, KC_RGUI, KC_RCTL },
- }
-
-/* SYSTEM
+  { KC_LCTL, KC_LGUI, KC_LALT, KC_APP,  KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_0,    KC_RALT, KC_TRNS, KC_TRNS, KC_RGUI, KC_RCTL },
+ },                    
+                       
+/* SYSTEM              
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * |        |        |        |        |        |        |        |        |        |        |        |        |        |        | RESET  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -103,12 +103,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
- [_SYS] = { /* SYSTEM */
-  { KC_NO, KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RESET }
-  { KC_NO, TO(_QW), KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }
-  { KC_NO, KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }
-  { KC_NO, KC_NO,   KC_NO, TO(_COL), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }
-  { KC_NO, KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }
+ [_KBD] = { /* KEYBOARD */
+  { KC_NO, KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RESET },
+  { KC_NO, TO(_QW), KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO },
+  { KC_NO, KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO },
+  { KC_NO, KC_NO,   KC_NO, TO(_COL), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO },
+  { KC_NO, KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO },
  }
 };
 
